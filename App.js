45967eea7b413b24b277a2855f39160ba0d1d4d2
Ahmad Saefudin <ahmaddin211056@gmail.com>
2022-06-23 15:30:59 +0700

    test update

--- App.js
+++ App.js
@@ -1,62 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import Animated, { concat, Extrapolate, interpolate, useAnimatedStyle, useSharedValue, Value, withSpring } from 'react-native-reanimated';
-import FlipCard from 'react-native-flip-card'
+import React, { Component } from "react";
+import * as THREE from "three";
+import { Renderer,TextureLoader } from 'expo-three';
+import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { Asset } from 'expo-asset';
+import * as FileSystem from 'expo-file-system';
 
-export default function App() {
 
-  const [index,setIndex] = useState(0);
-  const character = ["A","B","C","D"];
-  const color = ["red","green","blue","yellow"];
+export default function App(params) {
 
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+  const _onGLContextCreate = async gl => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      200, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000
+    );
+    const renderer = new Renderer({ gl });
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
 
-      <View style={{
-        width:200,
-        height:200,
-      }} >
-        <FlipCard
-        flip={true}
-          friction={6}
-          perspective={1000}
-          onFlipStart={()=>{
-            setIndex(index + 1 < 4 ? index + 1 : 0)
-          }}
-        >
-          {/* Face Side */}
-          <View style={{
-            backgroundColor:color[index],
-            width:200,
-            height:200,
-            alignItems:"center",
-            justifyContent:"center"
-          }}>
-            <Text style={{fontSize:100}} >{character[index]}</Text>
-          </View>
-          {/* Back Side */}
-          <View style={{
-            backgroundColor:color[index],
-            width:200,
-            height:200,
-            alignItems:"center",
-            justifyContent:"center"
-          }}>
-            <Text style={{fontSize:100}} >{character[index]}</Text>
-          </View>
-        </FlipCard>
-      </View>
-    </View>
-  );
+    const geometry = new THREE.SphereBufferGeometry(1, 36, 36);
+    const material = new THREE.MeshBasicMaterial({
+      map: new TextureLoader().load(require('./assets/texture.png'))
+    });
+    const sphere = new THREE.Mesh(geometry, material); 
+    
+    scene.add(sphere);
+    camera.position.z = 2;
+    const render = () => {
+      requestAnimationFrame(render);
+      sphere.rotation.x += 0.01;
+      sphere.rotation.y += 0.01;
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    };
+    render();
+  };
+
+
+  return(
+    <GLView
+      style={{ flex: 1 }}
+      onContextCreate={_onGLContextCreate}
+    >
+
+    </GLView>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
